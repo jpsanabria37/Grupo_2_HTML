4fdbfeef373825edf74d49a72aa44ad18f105864
jpsanabria37 <jpsanabria37@misena.edu.co>
2021-10-26 22:02:00 -0500

    Commit confirm

--- js/app.js
+++ js/app.js
@@ -24,23 +24,17 @@ function pintar(key) {
 
             break;
 
-<<<<<<< HEAD
-=======
->>>>>>> d071316110690f31e8b76f30e1eca4d1304861bf
         //Ejercicio 5
         case 5:
             document.getElementById("muestra").innerHTML = "<iframe src='./views/Ejercicio_5.html'></iframe>";
     
-<<<<<<< HEAD
-        break;
-    }
-=======
             break;
+    }
 
         //EJERCICIO 6
         case 6:
             document.getElementById("muestra").innerHTML = "<iframe src='./Ejercicio6.html'></iframe>";
+            
             break;
     }
 }
->>>>>>> d071316110690f31e8b76f30e1eca4d1304861bf
